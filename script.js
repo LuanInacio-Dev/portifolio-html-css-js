722c12112fde4f898ea21de05349c100bf1946fe
analistainácio <94462233+luaninacio755@users.noreply.github.com>
2024-01-15 22:17:40 -0400

    script.js
    Codigo Java script

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky ",this.window.scrollY > 120)
-})
+});
+ let menu = document.querySelector('#menu-icon');
+ let navlist = document.querySelector('.navlist')
+ //função do clique do botão que chama o menu
+ menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+     //função do clique do botão do menu ela fecha o menu pelo remove
+ };
+ window.onscroll  = () => {
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+    
+ };
